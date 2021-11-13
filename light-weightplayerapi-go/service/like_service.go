a5262352bdf71389d990bd1802643ddcc79222b7
XY <48501220+xueyikun@users.noreply.github.com>
2021-11-13 13:59:01 +0800

    后端代码（like_bug修复）

--- light-weightplayerapi-go/service/like_service.go
+++ light-weightplayerapi-go/service/like_service.go
@@ -3,6 +3,7 @@ package service
 import (
 	"github.com/gin-gonic/gin"
 	"singo/util"
+	"net/http"
 )
 
 type LikeService struct {
@@ -14,7 +15,14 @@ func (service *LikeService) Like(c *gin.Context) map[string]interface{} {
 
 	// 获得所有cookie
 	cookies := c.Request.Cookies()
-
+	cookies = append(cookies, &http.Cookie{
+		Name:  "appver",
+		Value: "2.7.1.198277",
+	   },
+		&http.Cookie{
+		 Name:  "os",
+		 Value: "pc",
+		})
 	options := &util.Options{
 		Crypto:  "weapi",
 		Cookies: cookies,
@@ -22,14 +30,14 @@ func (service *LikeService) Like(c *gin.Context) map[string]interface{} {
 	data := make(map[string]string)
 	data["alg"] = "itembased"
 	data["trackId"] = service.ID
-	if service.L != "" {
+	if service.L == "" {
 		data["like"] = "true"
 	} else {
 		data["like"] = service.L
 	}
-	data["time"] = "3"
+	data["time"] = "30"
 
-	reBody, cookies := util.CreateRequest("POST", `https://music.163.com/api/radio/like`, data, options)
+	reBody, _ := util.CreateRequest("POST", `https://music.163.com/api/radio/like`, data, options)
 
 	return reBody
 }
